8feb7d788a08d22a5696479d351d80548fd70bcd
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-03-21 11:39:25 -0500

    Adicion de "tsparticles" y "ng-particles"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgParticlesModule} from "ng-particles";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagErrorComponent } from './components/pag-error/pag-error.component';
+import { PagError2Component } from './components/pag-error2/pag-error2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagErrorComponent,
+    PagError2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
