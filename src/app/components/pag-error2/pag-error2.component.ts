c61078306a70e3bf3a21e48bc2cd58ccfa417cd6
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-03-21 13:07:06 -0500

    Creacion Pagina de Error Dino

---
+++ src/app/components/pag-error2/pag-error2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-error2',
+  templateUrl: './pag-error2.component.html',
+  styleUrls: ['./pag-error2.component.css']
+})
+export class PagError2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
