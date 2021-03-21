19df6c70a3b186b0bf24fffa6bc270d01313fcc1
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-03-21 13:07:16 -0500

    Creacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagErrorComponent } from './components/pag-error/pag-error.component';
+import { PagError2Component } from './components/pag-error2/pag-error2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Error404', component:PagErrorComponent},
+  {path:'Error',component:PagError2Component},
+  {path:'**',pathMatch:'full',redirectTo:'Error404'},
+  {path:'',pathMatch:'full',redirectTo:'Error404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
